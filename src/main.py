98eb67d6f9f9001748147003b4703d474d9fb1a2
MESHBotDev <95205434+MESHBotDev@users.noreply.github.com>
2021-11-29 09:07:58 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ async def get_text_messages(msg: types.Message):
     if msg.text.startswith("http"):
         answers = core.get_answers(link = msg.text)
         try:
-             for task_number, task in enumerate(answers):
+            for task_number, task in enumerate(answers):
                 await msg.answer(f"Вопрос №{task_number + 1}: {task[0]}\n\nОтвет: {task[1]}")
         except:
             await msg.answer(answers)
