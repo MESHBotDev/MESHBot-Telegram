23c55a3166e1e901a798814b8706aaf5a180c603
MESHBotDev <95205434+MESHBotDev@users.noreply.github.com>
2021-11-29 09:09:25 +0300

    Create searching_answers.py

---
+++ src/core/searching_answers.py
@@ -0,0 +1,9 @@
+import mesh
+
+def get_answers(link: str):
+    try:
+        answers = mesh.get_answers(link)
+        temp = answers[0]
+        return answers
+    except:
+        return "Я не могу найти ответы на этот тест. Проверьте правильность ссылки и попробуйте ещё раз."
