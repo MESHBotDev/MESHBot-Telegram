ad35b4a7ebeb32a11ee8720f060bae3930e99e03
paul9956 <84198877+paul9956@users.noreply.github.com>
2022-05-26 15:49:59 +0300

    Update searching_answers.py

--- src/core/searching_answers.py
+++ src/core/searching_answers.py
@@ -5,12 +5,14 @@ from functools import lru_cache
 @lru_cache(None)
 def get_answers(link: str):
     try:
-        result_answers = []
+#         result_answers = []
+        result_answers = None
         for all_answers in range(25):
             answers = mesh.get_answers(link)
-            for answer in answers:
-                if answer not in result_answers:
-                    result_answers.append(answer)
+            result_answers = [answer for answer in answers if answer not in result_answers]
+#             for answer in answers:
+#                 if answer not in result_answers:
+#                     result_answers.append(answer)
         return result_answers
     except:
         return '⚠️Хм странно, но я ничего не нашел. Проверь правильность ссылки или нажми 👉/help👈'
