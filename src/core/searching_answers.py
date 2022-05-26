46999b59053c7ae0dde968487553f3a4bf49fa28
paul9956 <84198877+paul9956@users.noreply.github.com>
2022-05-26 15:59:23 +0300

    Update searching_answers.py

--- src/core/searching_answers.py
+++ src/core/searching_answers.py
@@ -8,10 +8,10 @@ def get_answers(link: str):
         result_answers = []
         for all_answers in range(25):
             answers = mesh.get_answers(link)
-            result_answers = [answer for answer in answers if answer not in result_answers]
-#             for answer in answers:
-#                 if answer not in result_answers:
-#                     result_answers.append(answer)
+#             result_answers = [answer for answer in answers if answer not in result_answers]
+            for answer in answers:
+                if answer not in result_answers:
+                    result_answers.append(answer)
         return result_answers
     except:
         return '⚠️Хм странно, но я ничего не нашел. Проверь правильность ссылки или нажми 👉/help👈'
