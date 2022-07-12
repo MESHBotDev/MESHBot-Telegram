4fa6528ca3ee41445d159e9b277f8eefde8a82e4
Paul <84198877+paul9956@users.noreply.github.com>
2022-07-12 23:27:00 +0300

    Update searching_answers.py

--- src/core/searching_answers.py
+++ src/core/searching_answers.py
@@ -3,16 +3,10 @@ from functools import cache
 
 
 @cache
-def get_answers(link: str):
+def get_answers(link: str) -> list:
     try:
         result_answers = []
-        for all_answers in range(20):
-            answers = mesh.get_answers(link)
-            [result_answers.append(answer) for answer in answers if answer not in result_answers]
-#             for answer in answers:
-#                 if answer not in result_answers:
-#                     result_answers.append(answer)
-        print(result_answers)
+        [result_answers.append(answer) for _ in range(20) for answer in mesh.get_answers(link) if answer not in result_answers]
         return result_answers
     except:
         return '⚠️Хм странно, но я ничего не нашел. Проверь правильность ссылки или нажми 👉/help👈'
